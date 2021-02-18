d7efd5f09a02f670fee8594ecf33d492650cbc68
pranav1127 <74109716+pranav1127@users.noreply.github.com>
2021-02-17 20:16:47 -0500

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,35 @@
+    class Form{
+    constructor(){
+    this.input=createInput("name");
+    this.button=createButton('play');
+    this.greeting=createElement('h2');
+
+    }
+    hide(){
+        this.greeting.hide();
+        this.input.hide();
+        this.button.hide();
+    }
+    display(){
+    var title=createElement('h1');
+    title.html("Car Racing");
+    title.position(130,0);
+
+
+    this.input.position(130,160);
+    this.button.position(250,200);
+
+    this.button.mousePressed(function(){
+    this.input.hide();
+    this.button.hide();
+     player.name= this.input.value();
+    playerCount=playerCount +1;
+    player.index=playerCount;
+    player.update();
+    player.updateCount(playerCount);
+    this.greeting.html("hello"+player.name);
+    this.greeting.position(130,100);
+    })
+
+
+    }}
---
+++ sketch.js
@@ -0,0 +1,30 @@
+var form,player,game, database;
+var gameState=0;
+var playerCount;
+var allPlayers;
+var distance=0;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+game=new Game();
+   game.getState();
+game.start();
+
+  
+}
+
+function draw(){
+  background("white");
+  if (playerCount===4){
+game.update(1);
+
+  }
+   if (gameState===1) {
+clear();
+game.play();
+   }
+  
+}
+
